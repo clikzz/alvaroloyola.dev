09d54daa515d4e5aa4490790f98f5b857ca626de
Alvaro Loyola <alv.loyolamendoza@gmail.com>
2026-02-17 00:01:39 -0300

    feat: add `Navigation` component for site navigation with dynamic header styling.

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -21,8 +21,8 @@ export const Navigation: React.FC = () => {
 		<header ref={ref}>
 			<div
 				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${isIntersecting
-						? "bg-zinc-900/0 border-transparent"
-						: "bg-zinc-900/500  border-zinc-800 "
+					? "bg-zinc-900/0 border-transparent"
+					: "bg-zinc-900/500  border-zinc-800 "
 					}`}
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
@@ -38,6 +38,12 @@ export const Navigation: React.FC = () => {
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
 							Contacto
+						</Link>
+						<Link
+							href="/cv.pdf"
+							className="duration-200 text-zinc-400 hover:text-zinc-100"
+						>
+							CV
 						</Link>
 					</div>
 
